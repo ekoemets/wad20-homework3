f671dde7fe7acc77947b0412b044d5a3904824c1
kpekk <kristjan.pekk@gmail.com>
2020-11-20 13:41:10 +0200

    "Browse" tab valmis

--- src/components/ProfileCard.vue
+++ src/components/ProfileCard.vue
@@ -1,13 +1,73 @@
 <template>
-  
+    <div class="account">
+        <img v-bind:src="profile.avatar" alt="avatar">
+            <h2>{{profile.firstname}} {{profile.lastname}}</h2>
+            <button 
+                class = "followbutton"
+                v-bind:class="{ followed: isFollowed }"
+                @click="toggleFollow()"
+            >
+                Follow
+            </button>
+        </div>
 </template>
 
 <script>
 export default {
-    name: 'ProfileCard',
-}
+  name: "Profile",
+  props: {
+    profile: Object,
+  },
+  data: function () {
+    return {
+      isFollowed: false,
+    };
+  },
+  methods: {
+    toggleFollow: function () {
+      this.isFollowed = !this.isFollowed;
+    },
+  },
+};
 </script>
 
 <style>
+.account {
+    width: 250px;
+    flex: 1 0 40%;
+    margin: 1em 1em;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: center;
+    border: 1px solid rgb(168, 168, 168);
+    border-radius: 10px;
+}
+
+.followbutton {
+    margin-bottom: 2em;
+    padding: 12px 70px;
+    font-size: 1em;
+    background-color: #82008f;
+}
+
+.followbutton.followed {
+    background-color: white;
+    border: 1px solid #82008f;
+    color: #82008f;
+}
+
+.account>h2 {
+    margin: 1em 1em;
+}
+
+.account>img {
+    margin-top: 2em;
+    height: 120px;
+    width: 120px;
+    object-fit: cover;
+    object-position: 50% 10%;
+    border-radius: 50%;
+}
 
 </style>
--- src/pages/BrowsePage.vue
+++ src/pages/BrowsePage.vue
@@ -1,13 +1,21 @@
 <template>
-  <div>
-    <h1>Browse</h1>
-    <div v-for="profile in profiles" :key=profile.id>{{profile}}</div>
+  <div class = "container">
+    <div class = "accontainer" >
+    <div v-for="profile in profiles" :key=profile.id>
+      <Profile :profile="profile"></Profile>
+    </div>
+    </div>
   </div>
+  
 </template>
 
 <script>
+import Profile from "../components/ProfileCard";
 export default {
   name: "BrowsePage",
+  components: {
+    Profile
+  },
   mounted() {
     this.$store.dispatch("FETCH_PROFILES");
   },
@@ -20,4 +28,28 @@ export default {
 </script>
 
 <style>
+.container {
+  background: #ffffff;
+  width: 50%;
+  padding-top: 40px;
+  margin-left: auto;
+  margin-right: auto;
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+  
+}
+.accontainer {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+}
+@media only screen and (max-width: 600px) {
+  .container {
+    width: 100%;
+    padding: 0;
+  }
+}
+
+
 </style>
